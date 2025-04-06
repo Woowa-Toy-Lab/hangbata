7352ba70f1fa41744aeecad34bce62f52f506503
mun-kyeong <horse6953@naver.com>
2025-04-06 17:02:50 +0900

    feat : text 스타일 css 적용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 console.log("npm run dev 명령어를 통해 행바타 서버를 실행하세요.");
 
-addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const container = document.createElement("div");
-  container.textContent = "행바타";
+const app = document.querySelector("#app");
+const container = document.createElement("div");
+container.classList.add("tx-xxl");
+container.textContent = "행바타ㅏㅏㅏㅏㅏㅏㅏㅏ";
 
-  if (app) {
-    app.appendChild(container);
-  }
-});
+if (app) {
+  app.appendChild(container);
+}
